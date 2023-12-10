707da23f9c4a45e34fbb9be856d0610d089b1c6d
OksanaLysynchuk <138123244+OksanaLysynchuk@users.noreply.github.com>
2023-12-10 22:30:50 +0200

    js-ht-1

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,10 @@
 function getElementWidth(content, padding, border) {
-  return content + padding * 2 + border * 2;
+  const borderBox = content + padding *2 + border *2;
+  const boxSize = Number.parseFloat(borderBox);
+  return boxSize;
 }
+
+
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
